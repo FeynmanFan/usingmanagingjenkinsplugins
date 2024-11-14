aa2ae588393112f6dc24f7efdc3fa6dc8df15d2a
FeynmanFan <cbehrens@gmail.com>
2024-11-14 15:47:30 -0600

    Updated prometheus.config with host ip

--- init.groovy
+++ init.groovy
@@ -1,23 +1,34 @@
-import jenkins.*
-import jenkins.model.* 
-import hudson.*
-import hudson.model.*
+# my global config
+global:
+  scrape_interval: 15s # Set the scrape interval to every 15 seconds. Default is every 1 minute.
+  evaluation_interval: 15s # Evaluate rules every 15 seconds. The default is every 1 minute.
+  # scrape_timeout is set to the global default (10s).
 
+# Alertmanager configuration
+alerting:
+  alertmanagers:
+    - static_configs:
+        - targets:
+          # - alertmanager:9093
 
-// find all plugins related to Bitbucket
+# Load rules once and periodically evaluate them according to the global 'evaluation_interval'.
+rule_files:
+  # - "first_rules.yml"
+  # - "second_rules.yml"
 
-def pm = Jenkins.instance.pluginManager
+# A scrape configuration containing exactly one endpoint to scrape:
+# Here it's Prometheus itself.
+scrape_configs:
+  # The job name is added as a label `job=<job_name>` to any timeseries scraped from this config.
+  - job_name: "prometheus"
 
-def bbPlugins = pm.plugins.findAll { plugin ->
-  def description = plugin.getManifest().getMainAttributes().getValue("Long-Name")
-  description && description.toLowerCase().contains("bitbucket")
-}
+    # metrics_path defaults to '/metrics'
+    # scheme defaults to 'http'.
 
-// iterate over the list and disable each one
+    static_configs:
+      - targets: ["localhost:9090"]
+  - job_name: "jenkins"
+    metrics_path: "/prometheus"
 
-bbPlugins.each { plugin ->
-  println("Disabling ${plugin.getShortName()}")
-  plugin.disable()
-}
-
-Jenkins.instance.save()
+    static_configs:
+      - targets: ["host.docker.internal:2113"]
